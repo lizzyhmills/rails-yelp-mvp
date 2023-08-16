739ac75a12e4c1f4728504b7512448c4f18cb26e
Lizzy Mills <lizzyhmills@gmail.com>
2023-08-16 16:00:23 +0100

    Functionality of yelp created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants"
+
+rest1 = Restaurant.create(name: "Nandos", address: "New York", category: "chinese")
+puts "#{rest1.name} created"
+
+rest2 = Restaurant.create(name: "McDonalds", address: "London", category: "italian")
+puts "#{rest2.name} created"
+
+rest3 = Restaurant.create(name: "Te La", address: "Newcastle", category: "french")
+puts "#{rest3.name} created"
+
+rest4 = Restaurant.create(name: "Tinkerbell", address: "Hackney", category: "chinese")
+puts "#{rest4.name} created"
+
+rest5 = Restaurant.create(name: "Quantum", address: "Texas", category: "belgian")
+puts "#{rest5.name} created"
+
+rest6 = Restaurant.create(name: "Roker Park", address: "London", category: "italian")
+puts "#{rest6.name} created"
+
+puts "Restaurants created"
